d57534f9d459e6929e2ecc144fa333a6e95a1984
mu1616 <mu1616@naver.com>
2020-04-18 03:28:03 +0900

    등급 변경 Aspect 코드 가독성좋게 수정

--- project_01/src/main/java/com/example/project_01/aop/SetRoleAspect.java
+++ project_01/src/main/java/com/example/project_01/aop/SetRoleAspect.java
@@ -27,13 +27,14 @@ public class SetRoleAspect {
 		Object result = joinPoint.proceed();
 		// after
 		Object[] args = joinPoint.getArgs();
-		MemberDTO memberDto = (MemberDTO) args[0]; //중요!
+		MemberDTO memberDto = (MemberDTO) args[0]; // 중요!
 		memberDto = memberDao.findById(memberDto.getMem_id());
-		if (memberDto.getMem_role().equals("ADMIN")) return result;
+		if (memberDto.getMem_role().equals("ADMIN"))
+			return result;
 		List<RoleDTO> roleList = memberDao.selectRole();
-		int role=0;
-		for(int i=0; i< roleList.size(); i++) {
-			if(roleList.get(i).getRole_name().equals(memberDto.getMem_role())) {
+		int role = 0;
+		for (int i = 0; i < roleList.size(); i++) {
+			if (roleList.get(i).getRole_name().equals(memberDto.getMem_role())) {
 				role = i;
 			}
 		}
@@ -46,24 +47,24 @@ public class SetRoleAspect {
 		RoleDTO beforeRole = null;
 		// role_idx == 1:ADMIN, 2:USER -> ... ->n ADMIN을 제외하고 숫자 올라갈수록 등급 높음
 		// Role down
-		for(int i=role; i > 1; i--) {
+		for (int i = role; i > 1; i--) {
 			RoleDTO currentRole = roleList.get(i);
-			if(memberDto.getMem_total() < currentRole.getRole_price()) {
+			if (memberDto.getMem_total() < currentRole.getRole_price()) {
 				role--;
-			}else {
+			} else {
 				break;
 			}
 		}
 		// Role up
-		for(int i=role+1; i<roleList.size(); i++) {
+		for (int i = role + 1; i < roleList.size(); i++) {
 			nextRole = roleList.get(i);
-			if(memberDto.getMem_total() > nextRole.getRole_price()) {
-				role = i;
+			if (memberDto.getMem_total() > nextRole.getRole_price()) {
+				role++;
 			} else {
 				break;
 			}
 		}
 		RoleDTO myRole = roleList.get(role);
-		memberDao.updateRole(memberDto.getMem_idx(),myRole.getRole_idx());
+		memberDao.updateRole(memberDto.getMem_idx(), myRole.getRole_idx());
 	}
 }
