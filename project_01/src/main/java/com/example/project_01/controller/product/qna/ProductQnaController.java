282056952b317239270becdf1163d5b87c6a828c
mu1616 <mu1616@naver.com>
2020-04-18 16:34:07 +0900

    상품문의 처리 컨트롤러 product컨트롤러에서 분리
    상품문의기능 contorller.product.qna.ProductQnaController에서 처리

---
+++ project_01/src/main/java/com/example/project_01/controller/product/qna/ProductQnaController.java
@@ -0,0 +1,87 @@
+package com.example.project_01.controller.product.qna;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.project_01.model.pagination.dto.PageDTO;
+import com.example.project_01.model.product.dao.ProductDAO;
+import com.example.project_01.model.product.dto.ProductEntity;
+import com.example.project_01.model.product.qna.dao.QnaDAO;
+import com.example.project_01.model.product.qna.dto.QnaDTO;
+import com.example.project_01.model.product.qna.dto.SearchQnaDTO;
+import com.example.project_01.service.pagination.PageService;
+import com.example.project_01.service.product.qna.QnaService;
+
+@Controller
+public class ProductQnaController {
+	@Autowired
+	QnaDAO qnaDao;
+	@Autowired
+	ProductDAO productDao;
+	@Autowired
+	PageService pageService;
+	@Autowired
+	QnaService qnaService;
+	
+	
+	@RequestMapping(value="/product/qna/{product_idx}" , method = RequestMethod.GET)
+	public String qnaGet(@PathVariable(value = "product_idx", required = false)int product_idx,
+			Model model) {
+		ProductEntity productEntity = productDao.selectOne(product_idx);
+		model.addAttribute("productEntity",productEntity);		
+		return "popup/productQnA";
+	}
+	
+	//QnA 등록
+	@ResponseBody
+	@RequestMapping(value="/product/qna", method = RequestMethod.POST)
+	public void qnaPost(QnaDTO qnaDto) {
+		qnaDao.insert(qnaDto);
+	}
+	
+	//QnA 내용 출력
+	@RequestMapping("/product/qna/qnaShow")
+	public String qnaShow(int currentPage, int qna_product, Model model) {
+		SearchQnaDTO searchQnaDto = new SearchQnaDTO();
+		searchQnaDto.setQna_product(qna_product);
+		int totalRecord = qnaDao.countByProduct(qna_product);
+		PageDTO pageDto = pageService.calPage(currentPage, 10, totalRecord, 5);
+		model.addAttribute("qna_pageDto",pageDto);
+		List<QnaDTO> qnaList = qnaService.selectQna(currentPage, 10, searchQnaDto);
+		model.addAttribute("qnaList",qnaList);
+		return "product/qnaTable";
+	}
+	
+	//Qna 비밀글 보기
+	@ResponseBody
+	@RequestMapping("/product/qna/showSecret")
+	public QnaDTO showSecret(int qna_idx, Principal principal) {
+		if(principal == null) return new QnaDTO();
+		QnaDTO qnaDto = qnaDao.selectOne(qna_idx, new SearchQnaDTO());
+		if(!principal.getName().equals(qnaDto.getQna_member())) return new QnaDTO();
+		return qnaDto;
+	}
+	
+	//Qna 삭제
+	@ResponseBody
+	@RequestMapping("/product/qna/delete")
+	public String deleteOne(Principal principal, int qna_idx)  {
+		String msg = null;
+		QnaDTO qnaDto = qnaDao.selectOne(qna_idx, new SearchQnaDTO());
+		if(principal.getName().equals(qnaDto.getQna_member())) {
+			qnaDao.deleteOne(qna_idx);
+			msg = "삭제하였습니다.";
+		}else {
+			msg = "권한없음";
+		}
+		return msg;
+	}
+}
