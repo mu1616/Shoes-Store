d590917a120d6d3eb6dcbff1a0f75e225fe73960
mu1616 <mu1616@naver.com>
2020-10-25 04:09:59 +0900

    줄바꿈, 탭 수정
    메서드 체이닝을 사용하는 부분의 코드의 가독성을 높이기 위해 줄 바꿈 및
    탭 설정을 수정하였습니다.

--- Shoes-Store/src/main/java/com/example/store/config/SecurityConfig.java
+++ Shoes-Store/src/main/java/com/example/store/config/SecurityConfig.java
@@ -16,44 +16,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
-    public SpringSecurityDialect springSecurityDialect(){
-        return new SpringSecurityDialect();
-    }
+	public SpringSecurityDialect springSecurityDialect() {
+		return new SpringSecurityDialect();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()     //antMatchers() -> 앞에꺼 우선 적용
-                	.antMatchers("/product/qna/qnaShow","/product/review/show").permitAll()
-                	.antMatchers("/product/qna/**", "/cart/**", "/order/**", "/product/review/**",
-                			"/member/info/**").authenticated()
-                	.antMatchers("/admin/**").hasRole("ADMIN")
-                    .antMatchers("/**","/static/**").permitAll()
-                    .anyRequest().authenticated();
-        		
-        http.csrf().disable();
-        http.formLogin()
-        		.loginPage("/member/login")
-        		.loginProcessingUrl("/loginProcess")
-        		.defaultSuccessUrl("/")
-        		.failureUrl("/member/login/fail")
-        		.successHandler(new LoginSuccessHandler("/"))
-        		.permitAll()
-        		.and();
-        http .logout()
-        	 .logoutUrl("/member/logout") 
-        	 .logoutSuccessUrl("/") 
-        	 .invalidateHttpSession(true);
-        
-        //로그인중이긴 한데(role은 있는데) 인가되지 않는 role 일때
-        http.exceptionHandling().accessDeniedPage("/access-denied"); 
-        http
-        .headers()
-           .frameOptions()
-              .sameOrigin();
-        
+		http.authorizeRequests() // antMatchers() -> 앞에꺼 우선 적용
+				.antMatchers("/product/qna/qnaShow", "/product/review/show").permitAll()
+				.antMatchers("/product/qna/**", "/cart/**", "/order/**", "/product/review/**", "/member/info/**").authenticated()
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/**", "/static/**").permitAll()
+				.anyRequest().authenticated();
 
-    }
+		http.csrf().disable();
+		
+		http.formLogin()
+				.loginPage("/member/login")
+				.loginProcessingUrl("/loginProcess")
+				.defaultSuccessUrl("/")
+				.failureUrl("/member/login/fail")
+				.successHandler(new LoginSuccessHandler("/"))
+				.permitAll()
+				.and();
+		
+		http.logout()
+				.logoutUrl("/member/logout")
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true);
+
+		// 로그인중이긴 한데(role은 있는데) 인가되지 않는 role 일때
+		http.exceptionHandling().accessDeniedPage("/access-denied");
+		http.headers().frameOptions().sameOrigin();
+
+	}
 }
